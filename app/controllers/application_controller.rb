c7602975cd96365d73d956bfed5921fc1ad272df
Azania Mokhampane <azaniam04@gmail.com>
2024-03-20 00:27:26 +0200

    feat: login and logout UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+# this method is available to all controllers, it will call 
+# set_current_user before any action is called
+   before_action :set_current_user
+
+   def set_current_user
+       if session[:user_id]
+          Current.user = User.find_by(id: session[:user_id])
+      end
+   end
 end
