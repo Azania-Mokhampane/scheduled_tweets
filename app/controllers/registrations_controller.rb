9c78d3c16bd72d5034b597702967ad4d3b1bbe94
Azania Mokhampane <azaniam04@gmail.com>
2024-03-19 23:12:34 +0200

    keep users signed in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,9 @@ class RegistrationsController < ApplicationController
     @user = User.new(user_params)
     # check if it saves user successfully
     if @user.save
+      # if we use the cookies method anyone can see the user id in the browser,
+      #  but if we use the session method, the user id is hashed in the session
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Successfully created account"
     else 
       # flash[:alert] = "Account creation failed"
