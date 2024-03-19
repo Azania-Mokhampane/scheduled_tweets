4ff7d18cfce10c217335f8d6b8fcc6b60299b782
Azania Mokhampane <azaniam04@gmail.com>
2024-03-19 23:46:00 +0200

    feat: logging in a user

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,13 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "about", to: "about#index"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
