535482f7a62f54b7f6ea8d803e9aa5a0b97e22a9
Azania Mokhampane <azaniam04@gmail.com>
2024-03-19 23:26:18 +0200

    feat: lougout users

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
   # Defines the root path route ("/")
